d605279c8e9bf88668371fb804568e73ea582658
Attornado <73835871+attornado@users.noreply.github.com>
2022-11-27 19:30:50 +0100

    Refactored some constants and fixed some minor bugs.
    Signed-off-by: Attornado <73835871+Attornado@users.noreply.github.com>

--- preprocessing/constants.py
+++ preprocessing/constants.py
@@ -9,7 +9,7 @@ DATA_PATH: final = "data"
 # Additional PDBs from AlphaFold-related constants
 UNIPROTS_KEY: final = "uniprotIDs"
 PDBS_KEY: final = "pdbIDs"
-PATH_PDBS: final = os.path.join(DATA_PATH, "alphafold", "pdbs.json")
+PATH_PDBS_JSON: final = os.path.join(DATA_PATH, "alphafold", "pdbs.json")
 
 
 # PSCDB-related constants
