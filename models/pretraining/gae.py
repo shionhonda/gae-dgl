3e9bd6f3d2d8db1cbf02ba47507879268828af79
Attornado <73835871+attornado@users.noreply.github.com>
2023-01-01 16:00:34 +0100

    Added docs to GAEV2 class.
    Signed-off-by: Attornado <73835871+Attornado@users.noreply.github.com>

--- models/pretraining/gae.py
+++ models/pretraining/gae.py
@@ -4,8 +4,15 @@ from torch_geometric.nn import GAE
 
 
 class GAEV2(GAE):
-
     def __init__(self, encoder: torch.nn.Module, decoder: Optional[torch.nn.Module] = None):
+        """
+        GAE sub-class with a simple forward function implemented.
+
+        :param encoder: The encoder network
+        :type encoder: torch.nn.Module
+        :param decoder: Optional[torch.nn.Module] = None
+        :type decoder: Optional[torch.nn.Module]
+        """
         super(GAEV2, self).__init__(encoder=encoder, decoder=decoder)
 
     def forward(self, x, adj, sigmoid: bool = True):
