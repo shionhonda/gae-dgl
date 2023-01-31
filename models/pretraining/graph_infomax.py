e10322ad73b720005ed9d1eb8bb874ae2141cd82
Attornado <73835871+Attornado@users.noreply.github.com>
2023-01-31 17:25:57 +0100

    Added early stopping to training
    Signed-off-by: Attornado <73835871+Attornado@users.noreply.github.com>

--- models/pretraining/graph_infomax.py
+++ models/pretraining/graph_infomax.py
@@ -8,6 +8,9 @@ from torch_geometric.loader import DataLoader
 from torch_geometric.data import Data
 
 
+def readout_function():
+    # TODO must implement summary_function
+    pass
 
 
 def random_sample_corruption(training_set: DataLoader, graph: Data) -> Data:
@@ -37,7 +40,7 @@ class DeepGraphInfomaxWrapper(torch.nn.Module):
                  out_channels: int,
                  encoder: torch.nn.Module,
                  normalize_hidden: bool = True,
-                 summary: Callable = None,
+                 readout: Callable = None,
                  corruption: Callable = None,
                  dropout: float = 0.0
                  ):
@@ -47,7 +50,7 @@ class DeepGraphInfomaxWrapper(torch.nn.Module):
         self.dgi = DeepGraphInfomax(
             hidden_channels,
             encoder,
-            summary,
+            readout,
             corruption
         )
 
@@ -72,3 +75,172 @@ class DeepGraphInfomaxWrapper(torch.nn.Module):
             summary = self.lin2(summary)
 
         return summary
+
+
+def train_step_D(model: VGAEv2, train_data: DataLoader, optimizer, device, use_edge_weight: bool = False,
+                    use_edge_attr: bool = False):
+    model.train()  # put the model in training mode
+
+    running_loss = 0.0  # running average loss over the batches
+    steps: int = 1
+
+    for data in iter(train_data):
+        data = data.to(device)  # move batch to device
+        optimizer.zero_grad()  # reset the optimizer gradients
+
+        # Encoder output
+        if use_edge_weight and use_edge_attr:
+            z = model.encode(data.x, data.edge_index, edge_attr=data.edge_attr, edge_weight=data.edge_weight)
+        elif use_edge_attr:
+            z = model.encode(data.x, data.edge_index, edge_attr=data.edge_attr)
+        elif use_edge_weight:
+            z = model.encode(data.x, data.edge_index, edge_weight=data.edge_weight)
+        else:
+            z = model.encode(data.x, data.edge_index)
+
+        loss = model.recon_loss(z, data.edge_index)  # reconstruction loss
+        kl_divergence_loss = (1 / data.num_nodes) * model.kl_loss()  # KL-divergence loss, should work as mean on nodes
+        loss = loss + kl_divergence_loss
+
+        loss.backward()  # gradient update
+        optimizer.step()  # advance the optimizer state
+
+        # Update running average loss
+        running_loss = running_loss + 1 / steps * (loss.item() - running_loss)
+        steps += 1
+
+    return float(running_loss)
+
+
+@torch.no_grad()
+def test_step_vgae(model: VGAEv2, val_data: DataLoader, device, use_edge_weight: bool = False,
+                   use_edge_attr: bool = False):
+    model.eval()  # put the model in evaluation mode
+
+    # Running average for loss, precision and AUC
+    running_val_loss = 0.0
+    running_auc = 0.0
+    running_precision = 0.0
+    steps: int = 1
+
+    for data in iter(val_data):
+        data = data.to(device)  # move batch to device
+
+        # Encoder output
+        if use_edge_weight and use_edge_attr:
+            z = model.encode(data.x, data.edge_index, edge_attr=data.edge_attr, edge_weight=data.edge_weight)
+        elif use_edge_attr:
+            z = model.encode(data.x, data.edge_index, edge_attr=data.edge_attr)
+        elif use_edge_weight:
+            z = model.encode(data.x, data.edge_index, edge_weight=data.edge_weight)
+        else:
+            z = model.encode(data.x, data.edge_index)
+
+        loss = model.recon_loss(z, data.edge_index)  # reconstruction loss
+        kl_divergence_loss = (1 / data.num_nodes) * model.kl_loss()  # KL-divergence loss, should work as mean on nodes
+        loss = loss + kl_divergence_loss
+        running_val_loss = running_val_loss + 1 / steps * (
+                loss.item() - running_val_loss)  # update loss running average
+
+        # Update AUC and precision running averages
+        neg_edge_index = negative_sampling(data.edge_index, z.size(0))
+        auc, avg_precision = model.test(z, pos_edge_index=data.edge_index, neg_edge_index=neg_edge_index)
+        running_auc = running_auc + 1 / steps * (auc - running_auc)
+        running_precision = running_precision + 1 / steps * (avg_precision - running_precision)
+        steps += 1
+
+    return float(running_val_loss), running_auc, running_precision
+
+
+def train_vgae(model: VGAEv2, train_data: DataLoader, val_data: DataLoader, epochs: int, optimizer,
+               experiment_path: str, experiment_name: str, use_edge_weight: bool = False, use_edge_attr: bool = False,
+               early_stopping_patience: int = EARLY_STOP_PATIENCE, early_stopping_delta: float = 0) -> torch.nn.Module:
+    # Move model to device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+
+    # Instantiate the summary writer
+    writer = SummaryWriter(f'{experiment_path}_{experiment_name}_{epochs}_epochs')
+
+    # Early-stopping monitor
+    checkpoint_path = os.path.join(f"{experiment_path}", "checkpoint.pt")
+    monitor = EarlyStopping(
+        patience=early_stopping_patience,
+        verbose=True,
+        delta=early_stopping_delta,
+        path=checkpoint_path
+    )
+
+    # Metric history trace object
+    mht = MetricsHistoryTracer(
+        metrics=['train_loss', 'val_loss', 'auc_val', 'avg_precision_val'],
+        name="VGAE training metrics"
+    )
+
+    for epoch in range(0, epochs):
+        # Do train step
+        train_loss = train_step_vgae(
+            model=model,
+            train_data=train_data,
+            optimizer=optimizer,
+            device=device,
+            use_edge_weight=use_edge_weight,
+            use_edge_attr=use_edge_attr
+        )
+
+        # Do validation step
+        val_loss, auc, avg_precision = test_step_vgae(
+            model=model,
+            val_data=val_data,
+            device=device,
+            use_edge_weight=use_edge_weight,
+            use_edge_attr=use_edge_attr
+        )
+
+        print('Epoch: {:d}, Train loss: {:.4f}, Validation loss {:.4f}, '
+              'AUC: {:.4f}, Average precision: {:.4f}'.format(epoch, train_loss, val_loss, auc, avg_precision))
+
+        # Tensorboard state update
+        writer.add_scalar('train_loss', train_loss, epoch)
+        writer.add_scalar('auc_val', auc, epoch)  # new line
+        writer.add_scalar('val_loss', val_loss, epoch)
+        writer.add_scalar('avg_precision_val', avg_precision, epoch)  # new line
+
+        # Check for early-stopping stuff
+        monitor(val_loss, model)
+        if monitor.early_stop:
+            print(f"Epoch {epoch}: early stopping, restoring model checkpoint {checkpoint_path}...")
+            break
+
+        # Metrics history update
+        mht.add_scalar('train_loss', train_loss)
+        mht.add_scalar('val_loss', val_loss)
+        mht.add_scalar('auc_val', auc)
+        mht.add_scalar('avg_precision_val', avg_precision)
+
+    # Plot the metrics
+    mht.plot_metrics(
+        ['train_loss', 'val_loss'],
+        figsize=FIGURE_SIZE_DEFAULT,
+        traced_min_metric='val_loss',
+        store_path=os.path.join(f"{experiment_path}", "loss.svg")
+    )
+
+    mht.plot_metrics(
+        ['auc_val'],
+        figsize=FIGURE_SIZE_DEFAULT,
+        traced_min_metric='auc_val',
+        store_path=os.path.join(f"{experiment_path}", "auc.svg")
+    )
+
+    mht.plot_metrics(
+        ['avg_precision_val'],
+        figsize=FIGURE_SIZE_DEFAULT,
+        traced_min_metric='avg_precision_val',
+        store_path=os.path.join(f"{experiment_path}", "avg_precision.svg")
+    )
+
+    # Load best model
+    model.load_state_dict(torch.load(checkpoint_path))
+
+    return model
--- training/pretraining/graph_infomax_train_script.py
+++ training/pretraining/graph_infomax_train_script.py
@@ -1,7 +1,8 @@
 from functools import partial
-from models.pretraining.graph_infomax import random_sample_corruption, DeepGraphInfomaxWrapper as DGI
+from models.pretraining.encoders import RevGATConvEncoder
+from models.pretraining.graph_infomax import random_sample_corruption, readout_function,  DeepGraphInfomaxWrapper as DGI
 
-# TODO get the training set correctly
+# TODO get the training set properly
 TRAINING_SET = None
 
 CORRUPTION_FUNC = partial(
@@ -9,10 +10,39 @@ CORRUPTION_FUNC = partial(
     TRAINING_SET
 )
 
+REV_GAT_ENCODER = RevGATConvEncoder(
+    in_channels,
+    hidden_channels,
+    out_channels,
+    num_convs: int = 1,
+    dropout: float = 0.0,
+    project: bool = False, root_weight: bool = True,
+                                                                                                                                                 aggr: Optional[Union[str, list[str], Aggregation]] = "mean",
+                                                                                                                                                                                                      num_groups: int = 2, normalize_hidden: bool = True
+
+)
+
 
 
 def main():
-    model = DGI
+    rev_gat_dgi = DGI(
+        in_channels=0,
+        hidden_channels=0,
+        out_channels=0,
+        encoder=REV_GAT_ENCODER,
+        normalize_hidden=True,
+        readout=readout_function,
+        corruption=CORRUPTION_FUNC,
+        dropout=0.0
+    )
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
